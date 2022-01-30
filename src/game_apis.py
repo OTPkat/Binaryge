63fa3351210cb6e36a8014cde5d3c53a79db916e
adam55 <adam.sitbon@gmail.com>
2022-01-30 11:31:33 +0100

    adding some more abstraction and order

--- src/game_apis.py
+++ src/game_apis.py
@@ -6,10 +6,18 @@ S = TypeVar("S", bound=Type[BaseModel])
 
 # todo fill with requests to cloud run and may need to be abstract soon.
 
+
 class GameAPI(Generic[S]):
     # define all the requests to API's here since they just mostly depend on schema for most of them. Can always
     # add specifics in the child class. but for the classical ones no need (create update delete).
-    pass
+    def create_duel(self, s: S) -> S:
+        pass
+
+    def update_duel(self, id_: int) -> S:
+        pass
+
+    def delete_duel(self, id_: int) -> S:
+        pass
 
 
 BinerdgeApi = GameAPI[BinerdgeDuelSchema]
